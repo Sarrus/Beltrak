7fdb920be6fa0044a3edcba01af7df57f802a53f
Michael Bell <wiz_boy_8@hotmail.co.uk>
2013-02-02 14:42:02 +0000

    initialised libs

--- Beltrak/beltrak/beltrak.ino
+++ Beltrak/beltrak/beltrak.ino
@@ -12,6 +12,12 @@ Programing started: 02/02/2013 at 14:08
 
 */
 
+//declarations of librarys
+  #include <LiquidCrystal.h>
+  
+//initialise librarys
+  LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
 void setup()
 {
 }
