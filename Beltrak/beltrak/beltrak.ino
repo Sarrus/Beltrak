a6b2dfa9171d29c4406961ef2e51396b0a857706
Michael Bell <wiz_boy_8@hotmail.co.uk>
2013-02-03 13:27:48 +0000

    added the first sensor reading and speed setting
    compiled after typeing each line, also created a testing array to
    simulate a setting input and a virtual sensor in the form of a boolean
    to simulate a sensor, the code was sent to the board with the
    instructions preset and a serial console was used to test when it
    elected that the conditions where met, after this a volt meter was used
    to test weather or not the PPD was changed when instructed to do so

--- Beltrak/beltrak/beltrak.ino
+++ Beltrak/beltrak/beltrak.ino
@@ -34,6 +34,13 @@ Programing started: 02/02/2013 at 14:08
   //the position in the instruction array
     int instSet; //the set of instructions to be folowed, eg. go to hawkhaven, this is set by the user through the menus
     int instPos; //the position in the instructions, when a condition is met and a state is changed this incriments, when instSet changesm, this becomes 0
+    boolean met; //set to true of a condition is met, the state is then changed.
+    
+  //virtual sensors
+    /*these are variables that are used to test the program before sensors are intoduced true means HIGH false means LOW, !remember
+    to change these in the code when introducing sensors!*/
+    boolean VS[5];
+    
 
 void setup()
 {
@@ -50,11 +57,15 @@ void setup()
     inst[0][0][0] = 'B'; //in block
     inst[0][0][1] = '4'; //4
     inst[0][0][2] = 'S'; //set speed to
-    inst[0][0][3] = '1'; //1
+    inst[0][0][3] = '2'; //1
     
   //initialise array positions
     instSet = 0;
     instPos = 0;
+    met = false;
+    
+  //initialise virtual sensors
+  VS[4] = true;
 }
 
 void loop()
@@ -73,12 +84,94 @@ these three are repeated endlessly untill power off */
 //section 1: read user input and output display
 
 //section 2: check position and instructions
+  /* section 2 is devided into two parts, part one checks if the given condition is met, part two carries out an instruction and moves
+  to the next condition if it is, when a condition is met, the variable 'met' is set to true, section two is only run if met is true
+  it sets met to false once it has been run*/
+  
+    //section 1
+      switch(inst[instSet][instPos][0]) //this reads position 0 of an instruction set, to see what the condition is
+      {
+        case 'B':
+        {
+          Serial.println("state is B!"); //this tells us that the board has red state B
+          
+          if(VS[(inst[instSet][instPos][1]) - 48] == true) //this reads the virtual sensor dictated by position 1
+          {
+            Serial.println("sensor high"); //this is run if the sensor is high or the VS is true
+            met = true; //the condition is met so this goes true
+          }
+          
+          break;
+        }
+      }
+      
+    //section 2
+      if(met)
+      {
+        switch(inst[instSet][instPos][2]) //reads position 2 in the instruction set
+        {
+          case 'S': //set the PPD to a set value
+          {
+            switch(inst[instSet][instPos][3]) //reads position 3 which contains the PPD setting
+            {
+              case '0':
+              {
+                PPD = 0; //sets the ppd to 0%
+                break;
+              }
+              
+              case '1':
+              {
+                PPD = 50; //sets the PPD to 50%
+                reverser = false; // sets the direction forward
+                break;
+              }
+              
+              case '2':
+              {
+                PPD = 100; //sets the PPD to 100%
+                reverser = false; // sets the direction forward
+                break;
+              }
+              
+              case '3':
+              {
+                PPD = 50; //sets the PPD to 50%
+                reverser = true; // sets the direction backwards
+                break;
+              }
+              
+              case '4':
+              {
+                PPD = 100; //sets the PPD to 100%
+                reverser = true; // sets the direction backwards
+                break;
+              }
+            }
+            break;
+          }
+        }
+        
+        
+        met = false; //this prevents the instruction from being run twice
+        //instPos++; //this moves to the next instruction set, it is commented out because there is only one instruction so far
+      }
 
 //section 3: output to track
   //apply the PPD to the train
     analogWrite(pinPD, int((PPD / 100) * 255));
     
-  //delay(1); //protective delay to be used whenever outputing serial information
+  //apply the reverser to the train
+    if (reverser)
+    {
+      digitalWrite(pinDIR, HIGH);
+    }
+    else
+    {
+      digitalWrite(pinDIR, LOW);
+    }
+    
+  delay(1); //protective delay to be used whenever outputing serial information
 }
 
 
