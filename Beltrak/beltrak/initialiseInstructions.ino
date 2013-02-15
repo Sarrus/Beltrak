60b1a4d5fd3191afcd941fdc1bf59d291bde9790
Michael Bell <wiz_boy_8@hotmail.co.uk>
2013-02-15 16:05:14 +0000

    created test set of instructions

---
+++ Beltrak/beltrak/initialiseInstructions.ino
@@ -0,0 +1,33 @@
+/*
+
+ BELTRAK
+ 
+ V1.0
+ 
+ Hornby trainset automation
+ 
+ By Michael Bell
+ 
+ Programing started: 02/02/2013 at 14:08
+ 
+ */
+ 
+void initialiseInstructions()
+{
+  inst[0][0][0] = 'W';  inst[0][0][1] = '1';  inst[0][0][2] = 'X';  
+  
+  inst[1][0][0] = 'W';  inst[1][0][1] = '1';  inst[1][0][2] = 'D';  inst[1][0][3] = '1';
+  inst[1][1][0] = 'W';  inst[1][1][1] = '0';  inst[1][1][2] = 'S';  inst[1][1][3] = '1';
+  inst[1][2][0] = 'W';  inst[1][2][1] = '3';  inst[1][2][2] = 'S';  inst[1][2][3] = '2';
+  inst[1][3][0] = 'B';  inst[1][3][1] = '1';  inst[1][3][2] = 'S';  inst[1][3][3] = '1';
+  inst[1][4][0] = 'B';  inst[1][4][1] = '2';  inst[1][4][2] = 'X';  
+  
+  inst[2][0][0] = 'W';  inst[2][0][1] = '1';  inst[2][0][2] = 'D';  inst[2][0][3] = '1';
+  inst[2][1][0] = 'W';  inst[2][1][1] = '0';  inst[2][1][2] = 'S';  inst[2][1][3] = '1';
+  inst[2][2][0] = 'W';  inst[2][2][1] = '3';  inst[2][2][2] = 'S';  inst[2][2][3] = '2';
+  inst[2][3][0] = 'B';  inst[2][3][1] = '1';  inst[2][3][2] = 'S';  inst[2][3][3] = '1';
+  inst[2][4][0] = 'B';  inst[2][4][1] = '2';  inst[2][4][2] = 'X';  
+  
+  
+}
+
