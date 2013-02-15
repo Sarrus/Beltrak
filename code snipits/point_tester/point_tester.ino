fd57143a181d8bfe56e7e7321bb5abe428df0f14
Michael Bell <wiz_boy_8@hotmail.co.uk>
2013-02-15 01:34:34 +0000

    added control of 3 relays

--- code snipits/point_tester/point_tester.ino
+++ code snipits/point_tester/point_tester.ino
@@ -1,20 +1,33 @@
 void setup()
 {
+  pinMode (0, OUTPUT);
+  pinMode (1, OUTPUT);
+  pinMode (2, OUTPUT);
   pinMode (3, OUTPUT);
   pinMode (12, OUTPUT);
 }
 
 void loop()
 {
-  analogWrite (3, 255);
-  delay (300);
-  analogWrite(3, 0);
-  delay(1000);
-  digitalWrite (12, HIGH);
-  
-  analogWrite (3, 255);
-  delay (300);
-  analogWrite(3, 0);
-  delay(1000);
-  digitalWrite (12, LOW);
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(i, HIGH);
+    analogWrite (3, 255);
+    delay (300);
+    digitalWrite(i, LOW);
+    
+    analogWrite(3, 0);
+    delay(1000);
+    digitalWrite (12, HIGH);
+    
+    digitalWrite(i, HIGH);
+    analogWrite (3, 255);
+    delay (300);
+    analogWrite(3, 0);
+    digitalWrite(i, LOW);
+    
+    delay(1000);
+    digitalWrite (12, LOW);
+    
+  }
 }
