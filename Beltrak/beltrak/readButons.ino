224765709086c7f5c774e43d6661aec1a42c43e5
Michael Bell <wiz_boy_8@hotmail.co.uk>
2013-02-19 11:03:04 +0000

    commented readButtons

--- Beltrak/beltrak/readButons.ino
+++ Beltrak/beltrak/readButons.ino
@@ -12,15 +12,27 @@
  
  */
 
+/*this function returns an intager value coresponding the the current button (or lack therof) being
+pressed, it only works when either 1 or 0 butons are being pressed, the intager values output are
+#defined at the begining of the program as rightOut upOut etc.
+
+the sensor board outputs a diferent voltage depending on which buton is pressed, these voltages are
+#defined at the begining of the program and are called upADC, downADC etc.
+
+the sensitivity (or hysteresis) is how far out the voltage can be on either side of the defined value,
+this allows for the tolerence of the resistors used producing the voltages
+
+the voltage used is stored in buttonVoltage*/
+
 int readButons()
 {
-  unsigned int buttonVoltage = analogRead(pinButtons);
-  if(buttonVoltage < (rightADC + ADCsensitivity))
+  unsigned int buttonVoltage = analogRead(pinButtons); //this reads the voltage and stores it
+  if(buttonVoltage < (rightADC + ADCsensitivity)) //the rightADC is 0 so we check that it is beneath 0 +the sensitivity
   {
-    return rightOut;
+    return rightOut; //this returns the button pressed
   }
 
-  else if(buttonVoltage >= (upADC - ADCsensitivity) && buttonVoltage <= (upADC + ADCsensitivity))
+  else if(buttonVoltage >= (upADC - ADCsensitivity) && buttonVoltage <= (upADC + ADCsensitivity)) //this checks the voltage is in range
   {
     return upOut;
   }
@@ -42,7 +54,7 @@ int readButons()
 
   else
   {
-    return noneOut;
+    return noneOut; //if the voltage matches none of these then we return that no button is being pressed
   }
 }
 
