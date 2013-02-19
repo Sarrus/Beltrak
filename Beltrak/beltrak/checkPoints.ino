6c3be317d67d23a83db6c64d9c4a46e9e51374d1
Michael Bell <wiz_boy_8@hotmail.co.uk>
2013-02-19 10:28:41 +0000

    commented checkPoints

--- Beltrak/beltrak/checkPoints.ino
+++ Beltrak/beltrak/checkPoints.ino
@@ -12,14 +12,18 @@
  
  */
 
+/*this function checks the status of the points and compares them to what they should be, if its diferent it
+changes the points to match*/
+
+
 void checkPoints()
 {
-  for (int i = 1; i < 11; i++)
+  for (int i = 1; i < 11; i++)  //goes through all the points
   {
-    if (pointState[i] != pointSwitch[i])
+    if (pointState[i] != pointSwitch[i]) //if the state doesent match what it should be
     {
-      setPoint(pointSwitch[i], i);
-      pointState[i] = pointSwitch[i];
+      setPoint(pointSwitch[i], i); //move the points
+      pointState[i] = pointSwitch[i]; //change the state
     }
   }
 }
