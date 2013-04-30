763303a577bc3013991facc5e70e46fb3e9aabc9
Michael Bell <wiz_boy_8@hotmail.co.uk>
2013-04-30 10:50:26 +0100

    commeted and added the second half to checksensor
    compiled to test

--- Beltrak/beltrak/checkSensor.ino
+++ Beltrak/beltrak/checkSensor.ino
@@ -1,22 +1,49 @@
-boolean checkSensor(int no)
+/*
+
+ BELTRAK
+ 
+ V1.0
+ 
+ Hornby trainset automation
+ 
+ By Michael Bell
+ 
+ Programing started: 02/02/2013 at 14:08
+ 
+ */
+
+/*this function takes in the number for a sensor and reads the apropriate sensor set then outputs a boolean
+to indicate weather or not the sensor is currently triggered*/
+
+
+boolean checkSensor(int no) //no is the number of the sensor that we are reading
 {
-  if (no < 6)
+  if (no < 6) //if no is less than 6 then it belongs to the first set of sensors
   {
-    int highSensor = readSensors(pinLowSensors);
+    int highSensor = readSensors(pinLowSensors); //we find out what the high sensor in this set is
     
-    if (highSensor == sensorNone)
+    if (highSensor == sensorNone) //if there isnt one we return false
       return false;
       
-    else if (highSensor == no)
-      return true;
+    else if (highSensor == no) //as this is the first set the number in the set and the overall number are the same
+      return true; //if they match we return true
       
     else
-      return false;  
+      return false;  //in all other cases we return false
     
   }
   
-  else if (no < 11)
+  else if (no < 11) //if no does not belong to the first set and is less than 11 then it belongs to the second set
   {
+    int highSensor = readSensors(pinHighSensors); //we find out what the high sensor in this set is
     
+    if (highSensor == sensorNone) //if there isnt one we return false
+      return false;
+      
+    else if (highSensor == (no - 5)) //in the second set the sensor number is equal to no - 5
+      return true; //if it maches then we return true
+      
+    else
+      return false; //in all other cases we return false
   }
 }
